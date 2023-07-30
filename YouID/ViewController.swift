50d4de606ce10daaa7921d01f60b55157483a710
Mas'ud <masud@youverify.co>
2023-07-30 21:58:58 +0100

    Initial Commit

---
+++ YouID/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouID
+//
+//  Created by Masud Onikeku on 30/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
